a580208196513724e6c6073c14125902cc429667
ToniBirat7 <gotofreelancing1@gmail.com>
2026-05-21 16:52:03 +0545

    docs: pointers with arrays

--- notes/c++_revise/pointers/pointer_increment.cpp
+++ notes/c++_revise/pointers/pointer_increment.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
-    int arr[] = {1,2,3,4,5}
+    int arr[] = {1,2,3,4,5};
 
-    printf("")
+    printf("Arr : %p\n", arr);
+
+    int* ptr = arr; // points to the address of the first element of arr
+
+    // printf("Position One: %d\n", *ptr);
+
+    // looping with array pointer 
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", *ptr);
+        printf("%p\n", ptr);
+        ptr++; // if any pointer is incremented then, it points to the next address of the array element
+    }
 }
