23b436f73058b87d066664daf0985c661ca0568b
ToniBirat7 <gotofreelancing1@gmail.com>
2026-05-27 15:52:40 +0545

    docs: double pointers

--- notes/c++_revise/pointers/pointer.cpp
+++ notes/c++_revise/pointers/pointer.cpp
@@ -5,7 +5,18 @@ int main() {
     int x = 42;
     int* p1 = &x;
 
-    printf("P1 Value: %")
+    printf("P1 Address: %p\n", p1);
+    printf("P1 Value: %d\n", *p1);
 
-    printf("Value: %d\n", ***ptr3);
+    int** p2 = &p1;
+
+    printf("P2 Address: %p\n", p2);
+    printf("P2 Value (Address of x): %p\n", *p2);
+    printf("P2 Deref (x): %d\n", **p2);
+
+    int*** p3 = &p2;
+
+    printf("P3 Address: %p\n", p3);
+    printf("P3 Value (Address of p1): %p\n", *p3);
+    printf("P3 Deref (x): %d\n", ***p3);
 }
