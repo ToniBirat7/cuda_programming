059ffe551d8df36eae77135f0a75ab5b95c940e0
ToniBirat7 <gotofreelancing1@gmail.com>
2026-06-01 16:31:38 +0545

    docs: macros and global

--- notes/c++_revise/macros_globalvar/macro_global.cpp
+++ notes/c++_revise/macros_globalvar/macro_global.cpp
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include <iostream>
 
-using namespace std;
+#define PI 3.14159
+#define NUM_THREADS 256
+#define MULTIPLY(a, b) (a * b) // Function-like macro
 
 int main() {
-    
+    double circle = PI * 2; 
+    int total = MULTIPLY(10, 5);
     return 0;
 }
